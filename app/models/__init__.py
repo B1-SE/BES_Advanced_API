1e492207b95958e7ffd56c1f9b6f60960bb65854
B1-SE <bajones2@coastal.edu>
2025-09-26 23:47:48 -0500

    updated

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,7 +5,7 @@ Models package for the mechanic shop application.
 from .customer import Customer
 from .mechanic import Mechanic
 from .service_ticket import ServiceTicket
-from .inventory import InventoryItem
+from .inventory import InventoryItem as Inventory
 from .member import Member
 
-__all__ = ["Customer", "Mechanic", "ServiceTicket", "InventoryItem", "Member"]
+__all__ = ["Customer", "Mechanic", "ServiceTicket", "Inventory", "Member"]
