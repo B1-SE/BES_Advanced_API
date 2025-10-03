5cac50cb4a86cab8cd9325b7dc6c10c2ef385338
B1-SE <bajones2@coastal.edu>
2025-10-02 21:18:02 -0500

    refactor: Remove obsolete app/routes directory

--- app/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Routes package for the mechanic shop application.
-"""
-from .calculations import calculations_bp
-from .members import members_bp
-
-# Export all blueprints
-__all__ = ["calculations_bp", "members_bp"]
