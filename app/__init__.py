4458790408dd180a83d388fe3c88df37a9902570
B1-SE <bajones2@coastal.edu>
2025-10-02 22:00:21 -0500

    refactoring updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 """
 Customer blueprint package.
 """
-from flask import Blueprint
 
 # The blueprint is now defined in routes.py to avoid circular imports.
 from .routes import customers_bp  # noqa: F401
